d437b35f0ef1efbb9715a4ed498de60b365116c1
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-02 08:55:53 +0300

    Add project files.

---
+++ SpotlightTest/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotlightTest
+{
+    public partial class MainWindow : Window
+    {
+        [DllImport("uxtheme.dll", EntryPoint = "#95")]
+        public static extern uint GetImmersiveColorFromColorSetEx(uint dwImmersiveColorSet, uint dwImmersiveColorType, bool bIgnoreHighContrast, uint dwHighContrastCacheMode);
+        [DllImport("uxtheme.dll", EntryPoint = "#96")]
+        public static extern uint GetImmersiveColorTypeFromName(IntPtr pName);
+        [DllImport("uxtheme.dll", EntryPoint = "#98")]
+        public static extern int GetImmersiveUserColorSetPreference(bool bForceCheckRegistry, bool bSkipCheckOnFail);
+
+        public Color GetThemeColor()
+        {
+            var colorSetEx = GetImmersiveColorFromColorSetEx(
+                (uint)GetImmersiveUserColorSetPreference(false, false),
+                GetImmersiveColorTypeFromName(Marshal.StringToHGlobalUni("ImmersiveStartSelectionBackground")),
+                false, 0);
+
+            var colour = Color.FromArgb((byte)((0xFF000000 & colorSetEx) >> 24), (byte)(0x000000FF & colorSetEx),
+                (byte)((0x0000FF00 & colorSetEx) >> 8), (byte)((0x00FF0000 & colorSetEx) >> 16));
+
+            return colour;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Background = new SolidColorBrush(GetThemeColor());
+            Text.Background = Background;
+            Text.Focus();
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            //Close();
+        }
+    }
+}
